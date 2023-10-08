dcba19feb0103ee968b60d8b996ba165da130c72
justojavier00 <justojavier@gmail.com>
2023-10-08 13:19:24 -0400

    the funtion now generates code

--- json_to_object.py
+++ json_to_object.py
@@ -1,35 +1,35 @@
+from dataclasses import dataclass
 import json
 
-def create_class(data,name):
+def create_class(data, name):
     # Create a new class with the dictionary keys as attributes
     data_ = {}
-    for key,val in data.items():
-        if type(val) == dict:
-            data_[key] = create_class(val,key)
+    for key, val in data.items():
+        if isinstance(val, dict):
+            data_[key] = key
         else:
-            data_[key] = type(val)
-            
-    cls = type(name, (object,), data_)
-    return cls
+            data_[key] = type(val).__name__
+    print(name)
+    # Create the class definition code
+    class_def = "@dataclass\nclass " + name + ":\n"
+    for key, val in data_.items():
+        class_def += "    " + key + ": " + val + "\n"
+    
+    return class_def
 
-def create_classes(json_file):
-    # Load the JSON file
-    with open(json_file) as f:
-        data = json.load(f)
+def create_classes(data):
     
     # Iterate over the dictionaries in the JSON file and create a class for each one
-    classes = []
-    stack = [("c",data)]
+    class_defs = ""
+    stack = [("MainClass", data)]
     while stack:
         key, value = stack.pop()
         if isinstance(value, dict):
             # If the value is a dictionary, create a class for it and add it to the stack
-            cls = create_class(value,key)
-            #setattr(cls, '__name__', key)
-            classes.append(cls)
-            stack.extend([(f'{key}.{k}', v) for k, v in value.items()])
+            class_def = create_class(value, key)
+            class_defs += class_def + "\n"
+            stack.extend([(f'{k}', v) for k, v in value.items()])
         elif isinstance(value, list):
             # If the value is a list, add each element to the stack
-            stack.extend([(f'{key}[{i}]', v) for i, v in enumerate(value)])
-
-    return classes
+            stack.extend([(f'{key}{i}', v) for i, v in enumerate(value)])
+    return class_defs
